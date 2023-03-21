a857ad13d5658bf11b3339abc4776c1cacb0c8a4
shobola faruq <shobolafaruq@gmail.com>
2023-03-21 13:18:46 +0100

    Querying Followed post using a database join

--- app/models.py
+++ app/models.py
@@ -229,6 +229,11 @@ class User(UserMixin, db.Model):
             return False
         return self.followers.filter_by(follower_id=user.id).first() is not None
 
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
+            .filter(Follow.follower_id == self.id)
+
     def __repr__(self):
         # return '<User %r>' % self.username
         return f"User('{self.username}', '{self.email}')"
