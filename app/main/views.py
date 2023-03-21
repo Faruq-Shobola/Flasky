5f13b674bd6cb1b07c5a9c9e247d2bd270e7f9b1
shobola faruq <shobolafaruq@gmail.com>
2023-03-21 13:06:21 +0100

    Followers on the profile page

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,7 @@ from ..models import User, Role, Permission, Post
 from ..email import send_email
 from . import main
 from .forms import PostForm, EditProfileForm, EditProfileAdminForm
-from ..decorators import admin_required
+from ..decorators import admin_required, permission_required
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -102,3 +102,67 @@ def edit(id):
         return redirect(url_for('.post', id=post.id))
     form.body.data = post.body
     return render_template('edit_post.html', form=form)
+
+
+@main.route('/follow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    if current_user.is_following(user):
+        flash('You are already following this user.')
+        return redirect(url_for('.user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are now following %s.' % username)
+    return redirect(url_for('.user', username=username))
+
+
+@main.route('/unfollow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    if not current_user.is_following(user):
+        flash('You are not following this user.')
+        return redirect(url_for('.user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following %s anymore.' % username)
+    return redirect(url_for('.user', username=username))
+
+
+@main.route('/followers/<username>')
+def followers(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    per_page= int(current_app.config['FLASK_POST_PER_PAGE'])
+    pagination = user.followers.paginate(page=page, per_page=per_page, error_out=False)
+    follows = [{'user': item.follower, 'timestamp': item.timestamp}
+                for item in pagination.items]
+    return render_template('followers.html', user=user, title="Followers of",
+                            endpoint='.followers', pagination=pagination, follows=follows)
+
+
+@main.route('/followed-by/<username>')
+def followed_by(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    per_page= int(current_app.config['FLASK_POST_PER_PAGE'])
+    pagination = user.followed.paginate(page=page, per_page=per_page, error_out=False)
+    follows = [{'user': item.followed, 'timestamp': item.timestamp}
+                for item in pagination.items]
+    return render_template('followers.html', user=user, title="Followed by",
+                            endpoint='.followed_by', pagination=pagination, follows=follows)
--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_POST_PER_PAGE = os.environ.get('FLASK_POST_PER_PAGE')
+    FLASK_POST_PER_PAGE = os.environ.get('FLASK_POST_PER_PAGE') or 20
+    FLASK_FOLLOWERS_PER_PAGE = os.environ.get('FLASK_FOLLOWERS_PER_PAGE') or 50
 
     @staticmethod
     def init_app(app):
