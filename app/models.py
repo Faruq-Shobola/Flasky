292c505706754729f1a834f67cabdde464bf3a50
shobola faruq <shobolafaruq@gmail.com>
2023-03-21 14:00:13 +0100

    Making all users self-followers

--- app/models.py
+++ app/models.py
@@ -97,6 +97,14 @@ class User(UserMixin, db.Model):
     followers = db.relationship('Follow', foreign_keys=[Follow.followed_id],
                                 backref=db.backref('followed', lazy='joined'), lazy='dynamic',
                                 cascade='all, delete-orphan')
+   
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -105,8 +113,10 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
-            if self.email is not None and self.avatar_hash is None:
-                self.avatar_hash = self.gravatar_hash()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
+        self.follow(self)
+
 
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
